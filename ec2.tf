b4916f9d2007703f22ea587e1b1dcbd554ef685f
balaguru-20 <balaguravaiah0606@gmail.com>
2025-04-29 18:51:35 +0530

    terraform module test for ec2

--- ec2.tf
+++ ec2.tf
@@ -1,4 +1,4 @@
-module "ec2" {
+module "ec2" { #we can name it which name we want for module
   source        = "../terraform-aws-ec2"
   sg_id         = "sg-08f49690bf42656e1" #If we given sg_id in source code, that will be overriden here
   instance_type = "t3.small"
